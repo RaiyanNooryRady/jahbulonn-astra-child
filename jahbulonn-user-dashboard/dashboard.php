af17ba585d19082c0a6d510422e5888130358f15
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-20 16:11:20 +0600

    add

--- jahbulonn-user-dashboard/dashboard.php
+++ jahbulonn-user-dashboard/dashboard.php
@@ -2,26 +2,24 @@
 /** Template Name: My Dashboard */
 include "header-user-dashboard.php";
 
-?>  
+?>
 <main class="jahbulonn-main" id="jahbulonn-dashboard">
 
-<?php require "dashboard-mobile-navbar.php"; ?>
-<!-- Sidebar for desktop -->
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-3 col-lg-2 d-none d-md-block sidebar">
-            <?php include "menu-items.php"; ?>
-        </div>
+    <?php require "dashboard-mobile-navbar.php"; ?>
+    <!-- Sidebar for desktop -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 col-lg-2 d-none d-md-block sidebar">
+                <?php include "menu-items.php"; ?>
+            </div>
 
-        <div class="col-md-9 col-lg-10 col-12 p-4">
-            <h1>Welcome, <?php  echo $current_user->display_name; ?>!</h1>
-            <p>This is your dashboard content area.</p>
+            <div class="col-md-9 col-lg-10 col-12 p-4">
+                <h1>Welcome, <?php echo $current_user->display_name; ?>!</h1>
+                <p>This is your dashboard content area.</p>
+            </div>
         </div>
     </div>
-</div>
-<?php require "dashboard-offcanvas.php"; ?>
+    <?php require "dashboard-offcanvas.php"; ?>
 </main>
 
 <?php include "footer-user-dashboard.php"; ?>
-
-
