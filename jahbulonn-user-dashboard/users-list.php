b3588e8b6cc7b9894953641437706f99d74eb359
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-25 20:24:32 +0600

    user photo update

--- jahbulonn-user-dashboard/users-list.php
+++ jahbulonn-user-dashboard/users-list.php
@@ -43,23 +43,23 @@ include "header-user-dashboard.php";
                                 <tbody>
                                     <?php
                                     global $wpdb;
-                                    
+
                                     // Pagination settings
                                     $users_per_page = 5;
                                     $current_page = isset($_GET['page_num']) ? max(1, intval($_GET['page_num'])) : 1;
                                     $offset = ($current_page - 1) * $users_per_page;
-                                    
+
                                     // Get total number of users
                                     $total_users = $wpdb->get_var("SELECT COUNT(*) FROM wp_users");
                                     $total_pages = ceil($total_users / $users_per_page);
-                                    
+
                                     // Get users for current page
                                     $users = $wpdb->get_results($wpdb->prepare(
                                         "SELECT * FROM wp_users LIMIT %d OFFSET %d",
                                         $users_per_page,
                                         $offset
                                     ));
-                                    
+
                                     foreach ($users as $user) {
                                         ?>
                                         <tr>
@@ -70,8 +70,10 @@ include "header-user-dashboard.php";
                                             $roles = $user_data->roles;
                                             echo htmlspecialchars(implode(', ', $roles));
                                             ?></td>
-                                            <td><img src="<?php echo get_avatar_url($user->ID); ?>"
-                                                    class="rounded-circle dev-crud-user-photo" alt="Profile Picture"></td>
+                                            <td><img src="<?php
+                                            $custom_picture = get_user_meta($user->ID, 'profile_picture', true);
+                                            echo $custom_picture ? $custom_picture : get_avatar_url($user->ID);
+                                            ?>" alt="Profile Picture" class="jahbulonn-user-photo" /></td>
                                             <td><a href="#" class="btn btn-primary btn-sm" data-bs-toggle="modal"
                                                     data-bs-target="#editUserModal<?php echo $user->ID; ?>"><i
                                                         class="bi bi-pencil-square"></i></a></td>
@@ -84,34 +86,36 @@ include "header-user-dashboard.php";
                                     ?>
                                 </tbody>
                             </table>
-                            
+
                             <!-- Pagination -->
-                            <?php if ($total_pages > 1) : ?>
-                            <nav aria-label="Page navigation" class="mt-4">
-                                <ul class="pagination justify-content-center">
-                                    <?php if ($current_page > 1) : ?>
-                                        <li class="page-item">
-                                            <a class="page-link" href="?page_num=<?php echo $current_page - 1; ?>" aria-label="Previous">
-                                                <span aria-hidden="true">&laquo;</span>
-                                            </a>
-                                        </li>
-                                    <?php endif; ?>
-                                    
-                                    <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
-                                        <li class="page-item <?php echo $i === $current_page ? 'active' : ''; ?>">
-                                            <a class="page-link" href="?page_num=<?php echo $i; ?>"><?php echo $i; ?></a>
-                                        </li>
-                                    <?php endfor; ?>
-                                    
-                                    <?php if ($current_page < $total_pages) : ?>
-                                        <li class="page-item">
-                                            <a class="page-link" href="?page_num=<?php echo $current_page + 1; ?>" aria-label="Next">
-                                                <span aria-hidden="true">&raquo;</span>
-                                            </a>
-                                        </li>
-                                    <?php endif; ?>
-                                </ul>
-                            </nav>
+                            <?php if ($total_pages > 1): ?>
+                                <nav aria-label="Page navigation" class="mt-4">
+                                    <ul class="pagination justify-content-center">
+                                        <?php if ($current_page > 1): ?>
+                                            <li class="page-item">
+                                                <a class="page-link" href="?page_num=<?php echo $current_page - 1; ?>"
+                                                    aria-label="Previous">
+                                                    <span aria-hidden="true">&laquo;</span>
+                                                </a>
+                                            </li>
+                                        <?php endif; ?>
+
+                                        <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                                            <li class="page-item <?php echo $i === $current_page ? 'active' : ''; ?>">
+                                                <a class="page-link" href="?page_num=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                            </li>
+                                        <?php endfor; ?>
+
+                                        <?php if ($current_page < $total_pages): ?>
+                                            <li class="page-item">
+                                                <a class="page-link" href="?page_num=<?php echo $current_page + 1; ?>"
+                                                    aria-label="Next">
+                                                    <span aria-hidden="true">&raquo;</span>
+                                                </a>
+                                            </li>
+                                        <?php endif; ?>
+                                    </ul>
+                                </nav>
                             <?php endif; ?>
                         </div>
                     </div>
